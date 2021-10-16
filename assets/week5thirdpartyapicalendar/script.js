41d6ef8ce5a973b1ac2ab88a397af8146fffa8e4
Ricardo Crossley <richi.escamilla@gmail.com>
2021-10-15 19:03:22 -0700

    added the javascript functions, figuring out how to add the save image button. working on css styling too

--- assets/week5thirdpartyapicalendar/script.js
+++ assets/week5thirdpartyapicalendar/script.js
@@ -20,8 +20,8 @@ let timeblockID = $("textarea[id*='timeblock']");
 function init() {
     currentMomentDate();
     renderEvents();
-    setBGcolors():
-}:
+    setBGcolors();
+};
 
 // gets current date and renders in jumbotron header
 function currentMomentDate() {
@@ -30,34 +30,78 @@ function currentMomentDate() {
 };
 
 // renders events pulled from local storage to DOM 
-
+function renderEvents() {
+    for (let i = 0; i < timeArr.length; i++) {
+        $('[id^=timeblock-]').each(function (i, v) {
+            $(v).val(localStorage.getItem(timeArr[i]));
+        })
+    }
+};
 
 // triggers click Handler for save buttons
+saveBtn.on('click', saveButtonClickHandler);
 
 // when button is clicked, pulls corresponding Time and Date values
-    
+function saveButtonClickHandler(event) {
     // keeps form from sending
-    
+    event.preventDefault();
     // sets Value to Time assosciated with the clcked save button
-
+    calEntryEventTime = $(this).attr('id').split('-')[1];
     // sets values to the user's input text.
-
-    // calls fimctopm tp stpre in local storage
-
+    calEntryEventText = $(this).siblings('textarea[name^="timeblock"]').val().trim();
+    // calls function to stpre in local storage
+    storeEvents();
+};
 
 // stores time and text values to loxa storage where (time= key) and (user's input text=value)
+function setBGcolors() {
+    localStorage.setItem(calEntryEventTime, calEntryEventText);
+};
 
 // updates timeblock classes and colors as time progresses 
+function setBGcolors() {
     // for each timeblock ID, 
-
+    timeblockID.each(function () {
     // split it to display the time contained at the end of the ID, 
-
+    calTimeBlock = $(this).attr('id').split('-')[1];
     // and conver it to a moment.js format, and then an integer
-
+    calTimeBlock = parseInt(moment(calTimeBlock, 'H').format('H'));
     // get moment.js time and format identically
+    currentTime = parseInt(moment().format('H'));
+
+    if (currentTime < calTimeBlock) {
+        $(this).removeClass('past present');
+        $(this).addClass('future');
+    } else if (currentTime === calTimeBlock) {
+        $(this).removeClass('past future');
+        $(this).addClass('present');
+    } else if (currentTime > calTimeBlock) {
+        $(this).removeClass('present future');
+        $(this).addClass('past');
+    } else {
+        console.log("Time Calculation Error");
+    }
+    })
+};
 
 // updates date and time and colors once per minute on the minute
+function setIntervalOnMinute() {
+    var currentDateSeconds = new Date().getSeconds();
+    if (currentDateSeconds == 0) {
+        setInterval(currentMomentDate, 60000);
+        setInterval(setBGcolors, 60000);
+    } else {
+        setTimeout(function () {
+            setIntervalOnMinute();
+        }, (60 - currentDateSeconds) * 1000);
+    }
+    currentMomentDate();
+    setBGcolors();
+};
+
+setIntervalOnMinute();
 
 // initializes page
+init();
 
 // lastly: add media queries to upate viewing stle on mobile and small screens
